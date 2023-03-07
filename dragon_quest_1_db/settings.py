25642960775c42fe90d4ce00bf388cd1dc99e07f
stevengvan <vansteven17@gmail.com>
2023-03-06 22:08:32 -0800

    configuration test to deploy on vercel

--- dragon_quest_1_db/settings.py
+++ dragon_quest_1_db/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=config('SECRET_KEY')
 DEBUG=config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '.vercel.app']
 
 
 # Application definition
--- dragon_quest_1_db/wsgi.py
+++ dragon_quest_1_db/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dragon_quest_1_db.settings")
 
 application = get_wsgi_application()
+app = application
