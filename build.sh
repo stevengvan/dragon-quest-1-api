942efdf172fa6cecf5b9078e1029c8462b221bf4
stevengvan <vansteven17@gmail.com>
2023-03-06 22:06:27 -0800

    updated build and removed static

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ python -m pip install -r requirements.txt
 echo "Make Migration..."
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-
-echo "Collect Static..."
-python manage.py collectstatic --noinput --clear
