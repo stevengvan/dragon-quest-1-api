6f6bc31ed714686ad4d0bf47c8502d9a1d289821
stevengvan <vansteven17@gmail.com>
2023-03-06 22:15:11 -0800

    config test 5

--- dragon_quest_1_db/wsgi.py
+++ dragon_quest_1_db/wsgi.py
@@ -14,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dragon_quest_1_db.settings")
 
 application = get_wsgi_application()
-app = application
